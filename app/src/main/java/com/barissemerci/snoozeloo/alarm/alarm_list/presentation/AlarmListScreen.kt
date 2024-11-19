290ec54291e5976e99b1a99a80454d80cc583238
Barış SEMERCİ <barissemerci3@gmail.com>
2024-11-19 20:41:58 +0300

    add tool bar to alarm list screen

--- app/src/main/java/com/barissemerci/snoozeloo/alarm/alarm_list/presentation/AlarmListScreen.kt
+++ app/src/main/java/com/barissemerci/snoozeloo/alarm/alarm_list/presentation/AlarmListScreen.kt
@@ -14,10 +14,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.barissemerci.snoozeloo.R
 import com.barissemerci.snoozeloo.alarm.alarm_list.presentation.components.EmptyListInfo
 import com.barissemerci.snoozeloo.alarm.alarm_list.presentation.components.SnoozelooFloatingActionButton
+import com.barissemerci.snoozeloo.core.presentation.SnoozelooToolBar
 import com.barissemerci.snoozeloo.ui.AddIcon
 import com.barissemerci.snoozeloo.ui.theme.SnoozelooTheme
 import org.koin.androidx.compose.koinViewModel
@@ -49,12 +49,22 @@ private fun AlarmListScreen(
                 iconSize = 25.dp,
                 onClick = { onAction(AlarmListAction.OnAddAlarmClick) }
             )
+        },
+        topBar = {
+            SnoozelooToolBar(
+                startContent = {
+                    Box {
+                        Text(text = stringResource(id = R.string.your_alarms))
+                    }
+                },
+            )
         }
     ) { padding ->
-        Column(modifier = Modifier
-            .padding(padding)
-            .padding(16.dp)) {
-            Text(text = stringResource(id = R.string.your_alarms), fontSize = 24.sp)
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .padding(16.dp)
+        ) {
 
 
             if (state.alarms.isEmpty()) {
