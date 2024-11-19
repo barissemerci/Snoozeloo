5a8e417b1f27bc547c670d1a4003485ca904ccd4
Barış SEMERCİ <barissemerci3@gmail.com>
2024-11-19 20:40:20 +0300

    add tool bar

---
+++ app/src/main/java/com/barissemerci/snoozeloo/core/presentation/SnoozelooToolBar.kt
@@ -0,0 +1,60 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.barissemerci.snoozeloo.core.presentation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.barissemerci.snoozeloo.ui.theme.SnoozelooTheme
+
+@Composable
+fun SnoozelooToolBar(
+    modifier: Modifier = Modifier,
+    onActionItemClick: () -> Unit = {},
+    actionItem: (@Composable () -> Unit)? = null,
+    onStartItemClick: () -> Unit = {},
+    startContent: (@Composable () -> Unit)? = null,
+) {
+    TopAppBar(
+        title = {
+            Box(  modifier = Modifier.clickable { onStartItemClick() }){
+                startContent?.invoke()
+            }
+
+        },
+        modifier = modifier,
+        actions = {
+            Box(
+                modifier = Modifier.clickable { onActionItemClick() }
+            ) {
+                actionItem?.invoke()
+            }
+
+        }
+    )
+}
+    @Preview
+    @Composable
+    private fun SnoozelooToolBarPreview() {
+        SnoozelooTheme {
+            SnoozelooToolBar(
+                onActionItemClick = {},
+                actionItem = {
+                    Box {
+
+                    }
+                },
+                startContent = {
+                    Box {
+                        Text(text = "Your Alarms")
+                        }
+                }
+            )
+        }
+
+    }
