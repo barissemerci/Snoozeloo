d9c787c096640e953ae7d582da31d62e87404dc1
Barış SEMERCİ <barissemerci3@gmail.com>
2024-11-24 16:05:57 +0300

    add filtering hour and minute text

--- app/src/main/java/com/barissemerci/snoozeloo/alarm/alarm_detail/presentation/AlarmDetailViewModel.kt
+++ app/src/main/java/com/barissemerci/snoozeloo/alarm/alarm_detail/presentation/AlarmDetailViewModel.kt
@@ -8,21 +8,50 @@ import androidx.lifecycle.ViewModel
 
 class AlarmDetailViewModel : ViewModel() {
     var state by mutableStateOf(AlarmDetailState())
-    private set
-    fun onAction(action: AlarmDetailAction){
-        when(action){
+        private set
+
+    fun onAction(action: AlarmDetailAction) {
+        when (action) {
             AlarmDetailAction.OnCancelClick -> {
                 println("Cancel Clicked")
             }
+
             is AlarmDetailAction.OnSaveClick -> {
 
                 println("Save Clicked with ${action.alarmHour} ${action.alarmMinute} ${action.alarmName}")
             }
 
-            AlarmDetailAction.OnChangeNameClick -> {}
+            AlarmDetailAction.OnNameClick -> {
+                state = state.copy(isNameEditing = !state.isNameEditing)
+            }
             is AlarmDetailAction.OnSaveAlarmNameClick -> {
             }
-        }
 
+            is AlarmDetailAction.OnHourChange -> {
+
+                val number = action.hour.toIntOrNull()
+                if (action.hour.isEmpty()) {
+                    state = state.copy(alarmHour = action.hour)
+
+                } else if (number != null && number in 0..23) {
+                    state = state.copy(alarmHour = action.hour)
+
+                }
+
+            }
+
+            is AlarmDetailAction.OnMinuteChange -> {
+
+                val number = action.minute.toIntOrNull()
+                if (action.minute.isEmpty()) {
+                    state = state.copy(alarmMinute = action.minute)
+
+                } else if (number != null && number in 0..59) {
+                    state = state.copy(alarmMinute = action.minute)
+
+                }
+            }
+
+        }
     }
 }
