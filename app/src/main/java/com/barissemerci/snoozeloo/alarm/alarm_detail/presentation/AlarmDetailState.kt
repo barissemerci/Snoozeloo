45b2e6168bb87960eb95310ad27e1cb6db10e853
Barış SEMERCİ <barissemerci3@gmail.com>
2024-11-24 16:06:25 +0300

    update field types to String

--- app/src/main/java/com/barissemerci/snoozeloo/alarm/alarm_detail/presentation/AlarmDetailState.kt
+++ app/src/main/java/com/barissemerci/snoozeloo/alarm/alarm_detail/presentation/AlarmDetailState.kt
@@ -1,9 +1,9 @@
 package com.barissemerci.snoozeloo.alarm.alarm_detail.presentation
 
-import androidx.compose.foundation.text.input.TextFieldState
 
 data class AlarmDetailState(
-    val alarmHour: TextFieldState = TextFieldState("00"),
-    val alarmMinute:TextFieldState = TextFieldState("00"),
-    val alarmName: TextFieldState = TextFieldState()
+    val alarmHour: String = "00",
+    val alarmMinute: String = "00",
+    val alarmName: String = "00",
+    val isNameEditing: Boolean = false
 )
